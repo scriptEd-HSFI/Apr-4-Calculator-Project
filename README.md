2e395095303c842acd915dc5275bb3dc4d780874
Grace Tan <gtan@wework.com>
2016-04-03 19:58:06 -0400

    readme + function defs

---
+++ README.md
@@ -0,0 +1,12 @@
+Open this repo up in Cloud9 and press "Run". You should see something
+like:
+
+
+
+The goal of this project is to make the calculator work! The division
+function has already been provided for you. You will be implementing the
+addition, multiplication, square root functions as well as a few others.
+Please implement them in the functions.js file. Check your work by
+playing with the calculator.
+
+
--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,39 @@ function divide(x,y)
   return x/y;
 }
 
-// You must implement the rest of the functions.
+// TODO: You must implement the rest of the functions.
+
+function multiply(x,y)
+{
+
+
+}
+
+function add(x,y)
+{
+
+
+}
+
+//x is the base number, y is the exponent
+//hint: check out Math.pow
+function power(x,y)
+{
+
+
+}
+
+function square_root(x)
+{
+
+}
+
+function logBase10(x)
+{
+
+}
+
+function generateRandomNumber()
+{
+
+}
